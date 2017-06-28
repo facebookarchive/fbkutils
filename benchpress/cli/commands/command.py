9e248657deee296d87499f68f7baff079bc8e2a0
Vinnie Magro <vmagro@fb.com>
2017-06-27 17:44:38 -0700

    Convert BaseCommand to ABC, remove unused name attribute

--- benchpress/cli/commands/command.py
+++ benchpress/cli/commands/command.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+from abc import ABCMeta, abstractmethod
 
-class BenchpressCommand:
-    name = None
+class BenchpressCommand(object, metaclass=ABCMeta):
 
+    @abstractmethod
     def populate_parser(self, parser):
         pass
 
+    @abstractmethod
     def run(self, args, jobs):
         pass
