a12be7708e529a2b44d0772b83ae3565e122e938
Dave Jones <davej@codemonkey.org.uk>
2019-06-27 11:17:01 -0400

    fix gcc 8 fallthrough warnings

--- netconsd/main.c
+++ netconsd/main.c
@@ -74,10 +74,7 @@ static void parse_arguments(int argc, char **argv, struct netconsd_params *p)
 			     "[-b mmsg_batch] [-p udp_listen_addr] [-u udp_listen_port] "
 			     "[-g '${interval}/${age}'] [output module path] "
 			     "[another output module path...]\n", argv[0]);
-
-			/*
-			 * Fall through
-			 */
+			/* fall through */
 		default:
 			exit(1);
 		}
